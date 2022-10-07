4b54220125cd8f006d44b09bf325c812f13ec806
IdaRoos <102700726+IdaRoos@users.noreply.github.com>
2022-10-07 15:02:38 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,54 @@
+package miniprojekt1;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+			
+			while (true) {
+			// TODO Auto-generated method stub
+			Scanner sc = new Scanner(System.in);
+			int random = (int) Math.round(Math.random() * 100);
+			//System.out.println(random); // See the answer
+			int guesses = 0;
+			System.out.println("Guess the number between 0 to 100");
+			int input = -1;
+	
+		
+
+			while (!guessGame(input,random)) {
+				
+				 input = sc.nextInt();
+				 guesses++;
+				
+
+					if (input > random ) {
+						System.out.println("Wrong, it's lower");
+					
+					}
+
+					else if (input < random) {
+						System.out.println("Wrong, it's higher");
+					}
+			}
+			
+			System.out.println("Correct! You guessed " + guesses + " times");
+				
+
+			}
+			}
+		
+
+		public static boolean guessGame(int x, int y) {
+
+			return (x == y);
+
+		}
+
+	
+	}
+
+
